9749983a1c8dc53253ba3a5f99d52ed631144b00
delmas007 <azertyazerty1ze@gmail.com>
2023-11-16 16:18:33 +0100

    smtp

--- contact.php
+++ contact.php
@@ -1,26 +1,58 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $email = "smtp.google.com";
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+require_once 'phpMailer/Exception.php';
+require 'phpMailer/PHPMailer.php';
+require 'phpMailer/SMTP.php';
 
-    $to = 'angamancedrick@gmail.com';
-    $subject = 'Envoi du formulaire de contact';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['message'])) {
+        $nom = $_POST['name'];
+        $email = $_POST['email'];
+        $message = htmlspecialchars($_POST['message']);
 
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+        $to = 'angamancedrick@gmail.com';
+        $subject = 'Message depuis le formulaire de contact';
 
-    $success = mail($to, $subject, $message, $headers);
+        $mail = new PHPMailer(true);
 
-    if ($success) {
-        echo json_encode(['success' => true]);
-    } else {
-        echo json_encode(['success' => false, 'error' => 'Erreur lors de l\'envoi de l\'e-mail.']);
+        try {
+            // Paramètres du serveur SMTP
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'angamancedrick@gmail.com'; // Mettez votre adresse Gmail ici
+            $mail->Password   = 'ztzg ixxg zzew bcxw'; // Mettez votre mot de passe Gmail ici
+            $mail->SMTPSecure = 'tls';
+            $mail->Port       = 587;
+
+            // Destinataire
+            $mail->setFrom($email, $nom);
+            $mail->addAddress($to);
+
+            // Contenu du message
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+
+            // Organisez le corps de l'e-mail avec un en-tête "Reply-To"
+            $mail->Body = '<h1>Message envoyé depuis la page Contact de GroupeLaroche.com</h1>
+                           <p>
+                           <b>Nom : </b>' . $nom . '<br>
+                           <b>Email : </b>' . $email . '<br>
+                           <b>Message : </b>' . $message . '</p>';
+
+            // Ajoutez un en-tête "Reply-To" avec l'adresse e-mail de l'expéditeur
+            $mail->addReplyTo($email, $nom);
+
+            $mail->send();
+
+            // Return a JSON response
+            echo json_encode(['success' => true, 'message' => 'Votre message a été envoyé avec succès.']);
+        } catch (Exception $e) {
+            // Return a JSON response with error details
+            echo json_encode(['success' => false, 'message' => 'Erreur lors de l\'envoi du message.', 'error' => $mail->ErrorInfo]);
+        }
     }
-} else {
-    // Ajoutez une réponse pour les requêtes non POST
-    echo json_encode(['success' => false, 'error' => 'Méthode de requête non autorisée.']);
 }
-
+?>
